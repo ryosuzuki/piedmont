b3ef746a62d90dfaefb697796d8289eb58c6d8c0
Ryo Suzuki <ryosuzk@gmail.com>
2016-03-13 10:35:48 -0600

    Create subtract.js in porotypie

--- public/js/draw.js
+++ public/js/draw.js
@@ -76,6 +76,7 @@ function drawSTL () {
   }
   // xhr.open( "GET", '/public/assets/test-1.stl', true );
   xhr.open( "GET", '/public/assets/noah-4.stl', true );
+  // if STL is binary
   xhr.responseType = "arraybuffer";
   xhr.send( null );
 }
