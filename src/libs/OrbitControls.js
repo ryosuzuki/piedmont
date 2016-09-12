fa97db3ab82a4f60a1683305fccf09968ac2a7b8
Ryo Suzuki <ryosuzk@gmail.com>
2016-09-12 13:38:34 -0600

    Add restart in OrbitControls

--- src/libs/OrbitControls.js
+++ src/libs/OrbitControls.js
@@ -791,6 +791,27 @@
 
     }
 
+    this.restart = function() {
+      state = STATE.NONE;
+      this.dispose();
+
+      this.domElement.addEventListener( 'contextmenu', contextmenu, false );
+
+      this.domElement.addEventListener( 'mousedown', onMouseDown, false );
+      this.domElement.addEventListener( 'mousewheel', onMouseWheel, false );
+      this.domElement.addEventListener( 'MozMousePixelScroll', onMouseWheel, false ); // firefox
+
+      this.domElement.addEventListener( 'touchstart', touchstart, false );
+      this.domElement.addEventListener( 'touchend', touchend, false );
+      this.domElement.addEventListener( 'touchmove', touchmove, false );
+
+      window.addEventListener( 'keydown', onKeyDown, false );
+
+      // force an update at start
+      this.update();
+    }
+
+
     this.domElement.addEventListener( 'contextmenu', contextmenu, false );
 
     this.domElement.addEventListener( 'mousedown', onMouseDown, false );
